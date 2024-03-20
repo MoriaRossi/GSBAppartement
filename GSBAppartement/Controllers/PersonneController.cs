e8f8acd2bea5689d4cac916c21e91584ee0cf0da
moria <guillaume.dionisio@outlook.fr>
2024-03-20 16:00:47 +0100

    progress

--- GSBAppartement/Controllers/PersonneController.cs
+++ GSBAppartement/Controllers/PersonneController.cs
@@ -3,10 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
 
 namespace GSBAppartement.Controllers
 {
-    class PersonneController
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PersonneController
     {
+
+        [HttpGet("")]
+        public async void GetPersonne()
+        {
+
+        }
     }
 }
