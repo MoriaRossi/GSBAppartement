3d771855876987648e28d60238be6ad2796c23a1
QuentinGouttaya <beatsgr3y@gmail.com>
2024-04-13 17:35:07 +0200

    test commit

---
+++ Controllers/ProprietaireController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using GSBAppartement.Domain.Personnes;
+using GSBAppartement.Repository.Interfaces;
+
+
+namespace GSBAppartement.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProprietaireController : ControllerBase
+    {
+        private readonly IProprietaireRepository _proprietaireRepository;
+
+        public ProprietaireController(IProprietaireRepository proprietaireRepository)
+        {
+            _proprietaireRepository = proprietaireRepository;
+        }
+
+
+
+
+    }
+}
---
+++ Infrastructure/Interfaces/IProprietaireRepository.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+
+using GSBAppartement.Domain.Personnes;
+
+namespace GSBAppartement.Repository.Interfaces
+{
+
+    public interface IProprietaireRepository
+    {
+
+    }
+}
+
+
+
